d0608ad9cd46d8392ce1e40bc33a4312ca74744c
sombriks <sombriks@gmail.com>
2019-01-24 02:11:14 -0300

    downgrading babel since vueify maintainer threw the towel

--- six-pits-web/src/main.js
+++ six-pits-web/src/main.js
@@ -1,3 +1,11 @@
 import "./main.css";
 import { version } from "../package.json";
 console.log(`This is six pits version [${version}]`);
+
+import Vue from "vue";
+
+new Vue({
+  el: "#app",
+  name: "six-pits",
+  render: r => r(require("./App.vue"))
+});
